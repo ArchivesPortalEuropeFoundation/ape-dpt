08db3cb1273e45e125e1f3ea809f682e6ab2912e
stefanpapp-ape <stefan.papp.ape@gmail.com>
2013-01-28 12:44:26 +0000

    Bug #229: Added checking method if the root element of the XSLT files equals "xsl:stylesheet" to XsltChecker
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-dpt/trunk@55 87c78061-3d99-4f1f-8d5c-9a005f5ba2cf

--- DPTutils/src/main/java/eu/apenet/dpt/utils/util/XsltChecker.java
+++ DPTutils/src/main/java/eu/apenet/dpt/utils/util/XsltChecker.java
@@ -6,7 +6,15 @@ import net.sf.saxon.s9api.XsltCompiler;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  * User: Yoann Moranville
@@ -23,6 +31,26 @@ public class XsltChecker {
             compiler.compile(new StreamSource(xslFile));
             return true;
         } catch(Exception e){
+            return false;
+        }
+    }
+
+    public static boolean isXsltFile(File file) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new FileInputStream(file));
+            if (doc.getDocumentElement().getNodeName().equals("xsl:stylesheet")) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SAXException ex) {
+            System.err.println("File " + file.getName() + " is not a valid XSLT file!");
+            return false;
+        } catch (IOException ex) {
+            return false;
+        } catch (ParserConfigurationException ex) {
             return false;
         }
     }
