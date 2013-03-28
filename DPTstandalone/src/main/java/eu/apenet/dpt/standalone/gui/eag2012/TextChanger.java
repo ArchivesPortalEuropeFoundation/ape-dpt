ff8d962ff7530cd3ef0965f792bb888461075b71
yoannmoranville <yoann@archivesportaleurope.net>
2013-03-28 10:26:28 +0000

    Merged from DPT-project-1.2.0 Fix #330 [from revision 222] New modification for EAG 2012 and recordid [from revision 223] Fix #320 [from revision 224]
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-dpt/trunk@328 87c78061-3d99-4f1f-8d5c-9a005f5ba2cf

---
+++ DPTstandalone/src/main/java/eu/apenet/dpt/standalone/gui/eag2012/TextChanger.java
@@ -0,0 +1,24 @@
+package eu.apenet.dpt.standalone.gui.eag2012;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
+
+/**
+ * User: Yoann Moranville
+ * Date: 13/03/2013
+ *
+ * @author Yoann Moranville
+ */
+public abstract class TextChanger {
+    public static String getNewText(List<TextFieldWithCheckbox> textFieldWithCheckboxs, String countryCode) {
+        String returnValue = "";
+        for(TextFieldWithCheckbox textFieldWithCheckbox : textFieldWithCheckboxs) {
+            if(Eag2012ValidFields.isRepositoryCodeCorrect(textFieldWithCheckbox.getTextFieldValue()) && textFieldWithCheckbox.getisilOrNotComboValue().equals("ISIL"))
+                return textFieldWithCheckbox.getTextFieldValue();
+            else if(StringUtils.isNotEmpty(countryCode))
+                returnValue = countryCode + "-" + "99999999999";
+        }
+        return returnValue;
+    }
+}
