3642ec7b9d4ab2737f0e72423fdd0b0a9c07118c
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-22 06:11:38 +0000

    git-svn-id: https://build.archivesportaleurope.net/svn/apex-dpt/trunk@716 87c78061-3d99-4f1f-8d5c-9a005f5ba2cf

--- DPTstandalone/src/main/resources/production/launch.bat
+++
@@ -1,1 +0,0 @@
-java -Xms128m -Xmx512m -jar APE_data_preparation_tool-jar-with-dependencies.jar
--- DPTstandalone/src/main/resources/production/launch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Xms128m -Xmx512m -jar APE_data_preparation_tool-jar-with-dependencies.jar
