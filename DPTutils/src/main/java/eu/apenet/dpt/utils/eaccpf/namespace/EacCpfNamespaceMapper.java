bdab8358dcc2db530b567602024b0f6acd3a4ea1
stefanpapp-ape <stefan.papp.ape@gmail.com>
2014-04-23 14:31:43 +0000

    [DPTutils] updated EacCpfNamespaceMapper with xlink namespace
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-dpt/trunk@1102 87c78061-3d99-4f1f-8d5c-9a005f5ba2cf

--- DPTutils/src/main/java/eu/apenet/dpt/utils/eaccpf/namespace/EacCpfNamespaceMapper.java
+++ DPTutils/src/main/java/eu/apenet/dpt/utils/eaccpf/namespace/EacCpfNamespaceMapper.java
@@ -3,15 +3,17 @@ package eu.apenet.dpt.utils.eaccpf.namespace;
 import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
 
 /**
- * This class manage the EAC-CPF's name space 
+ * This class manage the EAC-CPF's name space
  *
  */
 public class EacCpfNamespaceMapper extends NamespacePrefixMapper {
 
     private static final String PREFIX = "";
     private static final String XSI_PREFIX = "xsi";
+    private static final String XLINK_PREFIX = "xlink";
     public static final String EAC_CPF_URI = "urn:isbn:1-931666-33-4";
     public static final String XSI_URI = "http://www.w3.org/2001/XMLSchema-instance";
+    public static final String XLINK_URI = "http://www.w3.org/1999/xlink";
 
     @Override
     public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix) {
@@ -19,13 +21,15 @@ public class EacCpfNamespaceMapper extends NamespacePrefixMapper {
             return PREFIX;
         } else if(XSI_URI.equals(namespaceUri)) {
             return XSI_PREFIX;
+        } else if(XLINK_URI.equals(namespaceUri)) {
+            return XLINK_PREFIX;
         }
         return suggestion;
     }
 
     @Override
     public String[] getPreDeclaredNamespaceUris() {
-        return new String[] {EAC_CPF_URI, XSI_URI};
+        return new String[] {EAC_CPF_URI, XSI_URI, XLINK_URI};
     }
 
 }
