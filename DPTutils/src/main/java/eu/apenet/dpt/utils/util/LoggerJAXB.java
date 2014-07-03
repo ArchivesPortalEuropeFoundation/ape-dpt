7bcac620630a02b2d70d0cf5a45f17ef5076a8ce
yoannmoranville <yoann@archivesportaleurope.net>
2014-07-03 10:27:41 +0000

    Add logger for JAXB and others
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-dpt/branches/DPT-project-1.6@1241 87c78061-3d99-4f1f-8d5c-9a005f5ba2cf

---
+++ DPTutils/src/main/java/eu/apenet/dpt/utils/util/LoggerJAXB.java
@@ -0,0 +1,44 @@
+package eu.apenet.dpt.utils.util;
+
+import eu.apenet.dpt.utils.eaccpf.MultipleIdentities;
+import org.apache.log4j.Logger;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.xpath.XPathFactory;
+import java.security.CodeSource;
+import java.text.MessageFormat;
+
+public class LoggerJAXB {
+    private static Logger logger = Logger.getLogger(LoggerJAXB.class);
+
+    public static void outputJaxpImplementationInfo() {
+        logger.info(getImplementationInfo("DocumentBuilderFactory", DocumentBuilderFactory.newInstance().getClass()));
+        logger.info(getImplementationInfo("XPathFactory", XPathFactory.newInstance().getClass()));
+        logger.info(getImplementationInfo("TransformerFactory", TransformerFactory.newInstance().getClass()));
+        logger.info(getImplementationInfo("SAXParserFactory", SAXParserFactory.newInstance().getClass()));
+        try {
+            logger.info(getImplementationInfo("JAXBContext", JAXBContext.newInstance(MultipleIdentities.class).getClass()));
+        } catch (JAXBException e) {
+            logger.error("Could not get information about the JAXBContext");
+        }
+    }
+
+    /**
+     * Get the JAXB implementation information for a particular class
+     * @param componentName
+     * @param componentClass
+     * @return
+     */
+    private static String getImplementationInfo(String componentName, Class componentClass) {
+        CodeSource source = componentClass.getProtectionDomain().getCodeSource();
+        return MessageFormat.format(
+                "{0} implementation: {1} loaded from: {2}",
+                componentName,
+                componentClass.getName(),
+                source == null ? "Java Runtime" : source.getLocation());
+    }
+}
