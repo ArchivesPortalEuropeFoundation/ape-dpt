f7cffcbced29961a412020c4f4daf9f0960b65f0
yoannmoranville <yoann@archivesportaleurope.net>
2014-11-19 12:23:05 +0000

    Fix bug
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-dpt/trunk@1565 87c78061-3d99-4f1f-8d5c-9a005f5ba2cf

--- DPTstandalone/src/main/java/eu/apenet/dpt/standalone/gui/XsltSelectorListener.java
+++ DPTstandalone/src/main/java/eu/apenet/dpt/standalone/gui/XsltSelectorListener.java
@@ -51,7 +51,7 @@ public class XsltSelectorListener implements ActionListener {
                 if(!fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAG_SCHEMA.getPath())))
                     fileInstance.setValidationSchema(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAG_2012_SCHEMA.getPath()));
             } else if(e.getActionCommand().equals("default-apeEAD.xsl")) {
-                if(!fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAD_SCHEMA.getPath())) && !fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD1_0_APE_SCHEMA.getPath())))
+                if(!fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAD_SCHEMA.getPath())))
                     fileInstance.setValidationSchema(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_APE_SCHEMA.getPath()));
             } else if(e.getActionCommand().equals("default-apeEAC-CPF.xsl")) {
                 fileInstance.setFileType(FileInstance.FileType.EAC_CPF);
