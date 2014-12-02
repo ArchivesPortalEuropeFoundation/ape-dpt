c930baab467206bc1ec1ebb770dbaea9bdf651bd
yoannmoranville <yoann@archivesportaleurope.net>
2014-12-02 16:25:57 +0000

    When selecting an XSL, all selected files will use that one: #1606
    git-svn-id: https://build.archivesportaleurope.net/svn/apex-dpt/trunk@1611 87c78061-3d99-4f1f-8d5c-9a005f5ba2cf

--- DPTstandalone/src/main/java/eu/apenet/dpt/standalone/gui/XsltSelectorListener.java
+++ DPTstandalone/src/main/java/eu/apenet/dpt/standalone/gui/XsltSelectorListener.java
@@ -41,24 +41,26 @@ public class XsltSelectorListener implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         JList list = dataPreparationToolGUI.getXmlEadList();
         if(list.getSelectedValue() != null){
-            FileInstance fileInstance = dataPreparationToolGUI.getFileInstances().get(((File)list.getSelectedValue()).getName());
-            fileInstance.setConversionScriptName(e.getActionCommand());
-            if(e.getActionCommand().equals("eac2eaccpf.xsl")){
-                fileInstance.setFileType(FileInstance.FileType.EAC_CPF);
-                fileInstance.setValidationSchema(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_APE_EAC_SCHEMA.getPath()));
-            } else if(e.getActionCommand().equals("eag2eag.xsl")){
-                fileInstance.setFileType(FileInstance.FileType.EAG);
-                if(!fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAG_SCHEMA.getPath())))
-                    fileInstance.setValidationSchema(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAG_2012_SCHEMA.getPath()));
-            } else if(e.getActionCommand().equals("default-apeEAD.xsl")) {
-                if(!fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAD_SCHEMA.getPath())))
-                    fileInstance.setValidationSchema(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_APE_SCHEMA.getPath()));
-            } else if(e.getActionCommand().equals("default-apeEAC-CPF.xsl")) {
-                fileInstance.setFileType(FileInstance.FileType.EAC_CPF);
-                if(!fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAC_SCHEMA.getPath())))
+            for(Object object : list.getSelectedValuesList()){
+                FileInstance fileInstance = dataPreparationToolGUI.getFileInstances().get(((File)object).getName());
+                fileInstance.setConversionScriptName(e.getActionCommand());
+                if(e.getActionCommand().equals("eac2eaccpf.xsl")){
+                    fileInstance.setFileType(FileInstance.FileType.EAC_CPF);
                     fileInstance.setValidationSchema(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_APE_EAC_SCHEMA.getPath()));
+                } else if(e.getActionCommand().equals("eag2eag.xsl")){
+                    fileInstance.setFileType(FileInstance.FileType.EAG);
+                    if(!fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAG_SCHEMA.getPath())))
+                        fileInstance.setValidationSchema(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAG_2012_SCHEMA.getPath()));
+                } else if(e.getActionCommand().equals("default-apeEAD.xsl")) {
+                    if(!fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAD_SCHEMA.getPath())))
+                        fileInstance.setValidationSchema(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_APE_SCHEMA.getPath()));
+                } else if(e.getActionCommand().equals("default-apeEAC-CPF.xsl")) {
+                    fileInstance.setFileType(FileInstance.FileType.EAC_CPF);
+                    if(!fileInstance.getValidationSchema().equals(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_EAC_SCHEMA.getPath())))
+                        fileInstance.setValidationSchema(Utilities.getXsdObjectFromPath(Xsd_enum.XSD_APE_EAC_SCHEMA.getPath()));
+                }
+                dataPreparationToolGUI.refreshButtons(fileInstance, Utilities.XSD_GROUP);
             }
-            dataPreparationToolGUI.refreshButtons(fileInstance, Utilities.XSD_GROUP);
         }
     }
 }
