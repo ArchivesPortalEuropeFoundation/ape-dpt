82466dea96c705cd082c8125209a4bbbd8d569a7
stefan <stefan.papp.ape@gmail.com>
2019-06-12 12:57:54 +0200

    DPTDEV-7

--- DPTstandalone/src/main/java/eu/apenet/dpt/standalone/gui/options/DigitalObjectAndRightsOptionFrame.java
+++ DPTstandalone/src/main/java/eu/apenet/dpt/standalone/gui/options/DigitalObjectAndRightsOptionFrame.java
@@ -17,7 +17,6 @@ package eu.apenet.dpt.standalone.gui.options;
  * See the Licence for the specific language governing permissions and limitations under the Licence.
  * #L%
  */
-
 import eu.apenet.dpt.standalone.gui.db.RetrieveFromDb;
 
 import javax.swing.*;
@@ -31,17 +30,17 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 /**
- * User: Yoann Moranville
- * Date: 16/02/2012
+ * User: Yoann Moranville Date: 16/02/2012
  *
  * @author Yoann Moranville
  */
 public class DigitalObjectAndRightsOptionFrame extends JFrame {
+
     private ResourceBundle labels;
     private RetrieveFromDb retrieveFromDb;
     private static boolean inUse = false;
 
-    public DigitalObjectAndRightsOptionFrame(ResourceBundle labels, RetrieveFromDb retrieveFromDb){
+    public DigitalObjectAndRightsOptionFrame(ResourceBundle labels, RetrieveFromDb retrieveFromDb) {
         inUse = true;
         this.labels = labels;
         this.retrieveFromDb = retrieveFromDb;
@@ -50,22 +49,23 @@ public class DigitalObjectAndRightsOptionFrame extends JFrame {
 
     public void createRoleTypeForm() {
         JPanel result = new JPanel();
-        result.setBorder( BorderFactory.createEmptyBorder( 10, 10, 10, 10 ) );
-        GroupLayout layout = new GroupLayout( result );
-        result.setLayout( layout );
-        layout.setAutoCreateGaps( true );
+        result.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        GroupLayout layout = new GroupLayout(result);
+        result.setLayout(layout);
+        layout.setAutoCreateGaps(true);
 
         String[] daos = {"TEXT", "IMAGE", "VIDEO", "SOUND", "3D", "UNSPECIFIED"};
         final JComboBox comboBox = new JComboBox<String>(daos);
         String defaultRoleType = retrieveFromDb.retrieveRoleType();
-        if(defaultRoleType == null) {
+        if (defaultRoleType == null) {
             comboBox.setSelectedIndex(5);
         } else {
             comboBox.setSelectedItem(defaultRoleType);
         }
         final JCheckBox useExistingCheckBox = new JCheckBox(labels.getString("ead.conversion.option.useExistingIfFound"));
-        if(retrieveFromDb.retrieveUseExistingRoleType())
+        if (retrieveFromDb.retrieveUseExistingRoleType()) {
             useExistingCheckBox.setSelected(true);
+        }
         JLabel label1 = new JLabel("<html><font size='5'>" + labels.getString("ead.conversion.option.dao") + "</font></html>");
         JSeparator separator1 = new JSeparator();
 
@@ -73,7 +73,7 @@ public class DigitalObjectAndRightsOptionFrame extends JFrame {
         String name = RightsInformation.getRightsInformationFromUrl(retrieveFromDb.retrieveDaoRights()).getName();
         comboBoxRightsDao.setSelectedItem(name);
         final JTextArea descriptionRightsDao = new JTextArea();
-        JScrollPane descriptionRightsDaoScroll = new JScrollPane (descriptionRightsDao, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        JScrollPane descriptionRightsDaoScroll = new JScrollPane(descriptionRightsDao, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         descriptionRightsDao.setText(retrieveFromDb.retrieveDaoRightsDesc());
         descriptionRightsDao.setRows(4);
         final JTextField rightsholderRightsDao = new JTextField();
@@ -87,7 +87,7 @@ public class DigitalObjectAndRightsOptionFrame extends JFrame {
         name = RightsInformation.getRightsInformationFromUrl(retrieveFromDb.retrieveEadRights()).getName();
         comboBoxRightsEad.setSelectedItem(name);
         final JTextArea descriptionRightsEad = new JTextArea();
-        JScrollPane descriptionRightsEadScroll = new JScrollPane (descriptionRightsEad, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        JScrollPane descriptionRightsEadScroll = new JScrollPane(descriptionRightsEad, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         descriptionRightsEad.setText(retrieveFromDb.retrieveEadRightsDesc());
         descriptionRightsEad.setRows(4);
         final JTextField rightsholderRightsEad = new JTextField();
@@ -96,75 +96,74 @@ public class DigitalObjectAndRightsOptionFrame extends JFrame {
         JLabel label6 = new JLabel(labels.getString("ead.conversion.option.rights.description"));
         JLabel label7 = new JLabel(labels.getString("ead.conversion.option.rights.rightsholder"));
 
-        layout.setHorizontalGroup( layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addComponent(label1)
-                                .addComponent(comboBox)
-                                .addComponent(useExistingCheckBox)
-                                .addComponent(separator1)
-                                .addGap(5)
-                                .addComponent(label2)
-                                .addComponent(comboBoxRightsDao)
-                                .addComponent(label3)
-                                .addComponent(descriptionRightsDaoScroll)
-                                .addComponent(label4)
-                                .addComponent(rightsholderRightsDao)
-                                .addComponent(separator2)
-                                .addGap(5)
-                                .addComponent(label5)
-                                .addComponent(comboBoxRightsEad)
-                                .addComponent(label6)
-                                .addComponent(descriptionRightsEadScroll)
-                                .addComponent(label7)
-                                .addComponent(rightsholderRightsEad))
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(label1)
+                        .addComponent(comboBox)
+                        .addComponent(useExistingCheckBox)
+                        .addComponent(separator1)
+                        .addGap(5)
+                        .addComponent(label2)
+                        .addComponent(comboBoxRightsDao)
+                        .addComponent(label3)
+                        .addComponent(descriptionRightsDaoScroll)
+                        .addComponent(label4)
+                        .addComponent(rightsholderRightsDao)
+                        .addComponent(separator2)
+                        .addGap(5)
+                        .addComponent(label5)
+                        .addComponent(comboBoxRightsEad)
+                        .addComponent(label6)
+                        .addComponent(descriptionRightsEadScroll)
+                        .addComponent(label7)
+                        .addComponent(rightsholderRightsEad))
         );
 
         layout.setVerticalGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(label1))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(comboBox))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(useExistingCheckBox))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(separator1))
-                        .addGap(5)
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(label2))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(comboBoxRightsDao))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(label3))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(descriptionRightsDaoScroll))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(label4))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(rightsholderRightsDao))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(separator2))
-                        .addGap(5)
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(label5))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(comboBoxRightsEad))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(label6))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(descriptionRightsEadScroll))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(label7))
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(rightsholderRightsEad))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(label1))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(comboBox))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(useExistingCheckBox))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(separator1))
+                .addGap(5)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(label2))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(comboBoxRightsDao))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(label3))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(descriptionRightsDaoScroll))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(label4))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(rightsholderRightsDao))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(separator2))
+                .addGap(5)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(label5))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(comboBoxRightsEad))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(label6))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(descriptionRightsEadScroll))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(label7))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(rightsholderRightsEad))
         );
-
 
         JButton showItButton = new JButton(labels.getString("ok"));
         showItButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String daoType = (String)comboBox.getSelectedItem();
+                String daoType = (String) comboBox.getSelectedItem();
                 retrieveFromDb.saveOrUpdateRoleType(daoType, useExistingCheckBox.isSelected());
-                retrieveFromDb.saveEadRights(RightsInformation.getRightsInformationFromName((String)comboBoxRightsEad.getSelectedItem()).getUrl(), descriptionRightsEad.getText(), rightsholderRightsEad.getText());
+                retrieveFromDb.saveEadRights(RightsInformation.getRightsInformationFromName((String) comboBoxRightsEad.getSelectedItem()).getUrl(), descriptionRightsEad.getText(), rightsholderRightsEad.getText());
                 retrieveFromDb.saveDaoRights(RightsInformation.getRightsInformationFromName((String) comboBoxRightsDao.getSelectedItem()).getUrl(), descriptionRightsDao.getText(), rightsholderRightsDao.getText());
                 closeFrame();
             }
@@ -214,7 +213,6 @@ public class DigitalObjectAndRightsOptionFrame extends JFrame {
     public enum RightsInformation {
         NO_SELECTION("", "---"),
         PUBLIC_DOMAIN_MARK("http://creativecommons.org/publicdomain/mark/1.0/", "Public Domain Mark"),
-        OUT_OF_COPYRIGHT("http://www.europeana.eu/rights/out-of-copyright-non-commercial/", "Out of copyright - no commercial re-use"),
         CREATIVECOMMONS_CC0_PUBLIC("http://creativecommons.org/publicdomain/zero/1.0/", "Creative Commons CC0 Public Domain Dedication"),
         CREATIVECOMMONS_ATTRIBUTION("http://creativecommons.org/licenses/by/4.0/", "Creative Commons Attribution"),
         CREATIVECOMMONS_ATTRIBUTION_SHARE("http://creativecommons.org/licenses/by-sa/4.0/", "Creative Commons Attribution, ShareAlike"),
@@ -222,13 +220,16 @@ public class DigitalObjectAndRightsOptionFrame extends JFrame {
         CREATIVECOMMONS_ATTRIBUTION_NON_COMERCIAL("http://creativecommons.org/licenses/by-nc/4.0/", "Creative Commons Attribution, Non-Commercial"),
         CREATIVECOMMONS_ATTRIBUTION_NC_SHARE("http://creativecommons.org/licenses/by-nc-sa/4.0/", "Creative Commons Attribution, Non-Commercial, ShareAlike"),
         CREATIVECOMMONS_ATTRIBUTION_NC_NO_DERIVATES("http://creativecommons.org/licenses/by-nc-nd/4.0/", "Creative Commons Attribution, Non-Commercial, No Derivatives"),
-        FREE_ACCESS_NO_REUSE("http://www.europeana.eu/rights/rr-f/", "Free access – no re-use"),
-        PAID_ACCESS_NO_REUSE("http://www.europeana.eu/rights/rr-p/", "Paid access – no re-use"),
-        ORPHAN_WORKS("http://www.europeana.eu/rights/orphan-work-eu/", "Orphan works"),
-        UNKNOWN("http://www.europeana.eu/rights/unknown/", "Unknown");
+        EUROPEANA_IN_COPYRIGHT("http://rightsstatements.org/vocab/InC/1.0/", "In Copyright"),
+        EUROPEANA_EDUCATIONAL_REUSE_PERMITTED("http://rightsstatements.org/vocab/InC-EDU/1.0/", "In Copyright - educational re-use permitted"),
+        EUROPEANA_EU_ORPHAN_WORK("http://rightsstatements.org/vocab/InC-OW-EU/1.0/", "In Copyright - EU Orphan work"),
+        EUROPEANA_NOCOPYRIGHT_NON_COMMERCIAL("http://rightsstatements.org/vocab/NoC-NC/1.0/", "No Copyright - non-commercial use only"),
+        EUROPEANA_NOCOPYRIGHT_OTHER_KNOWN_LEGAL_RESTRICTIONS("http://rightsstatements.org/vocab/NoC-OKLR/1.0/", "No Copyright - other known legal restrictions"),
+        EUROPEANA_COPYRIGHT_NOT_EVALUATED("http://rightsstatements.org/vocab/CNE/1.0/", "Copyright not evaluated");
 
         String url;
         String name;
+
         RightsInformation(String url, String name) {
             this.url = url;
             this.name = name;
@@ -244,15 +245,15 @@ public class DigitalObjectAndRightsOptionFrame extends JFrame {
 
         public static String[] toArray() {
             List<String> rightsInformationList = new ArrayList<String>();
-            for(RightsInformation rightsInformation : RightsInformation.values()) {
+            for (RightsInformation rightsInformation : RightsInformation.values()) {
                 rightsInformationList.add(rightsInformation.getName());
             }
             return rightsInformationList.toArray(new String[rightsInformationList.size()]);
         }
 
         public static RightsInformation getRightsInformationFromName(String name) {
-            for(RightsInformation rightsInformation : RightsInformation.values()) {
-                if(rightsInformation.getName().equals(name)) {
+            for (RightsInformation rightsInformation : RightsInformation.values()) {
+                if (rightsInformation.getName().equals(name)) {
                     return rightsInformation;
                 }
             }
@@ -260,8 +261,8 @@ public class DigitalObjectAndRightsOptionFrame extends JFrame {
         }
 
         public static RightsInformation getRightsInformationFromUrl(String url) {
-            for(RightsInformation rightsInformation : RightsInformation.values()) {
-                if(rightsInformation.getUrl().equals(url)) {
+            for (RightsInformation rightsInformation : RightsInformation.values()) {
+                if (rightsInformation.getUrl().equals(url)) {
                     return rightsInformation;
                 }
             }
